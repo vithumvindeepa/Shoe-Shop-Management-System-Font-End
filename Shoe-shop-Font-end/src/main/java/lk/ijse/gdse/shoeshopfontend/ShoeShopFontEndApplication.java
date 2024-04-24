c84738a5c7988bf153e5d2d9150ba6c768aaf1a7
vithumvindeepa <vindeepavithum@gmail.com>
2024-04-24 12:21:33 +0530

    first commit

---
+++ Shoe-shop-Font-end/src/main/java/lk/ijse/gdse/shoeshopfontend/ShoeShopFontEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.shoeshopfontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopFontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeShopFontEndApplication.class, args);
+    }
+
+}
---
+++ Shoe-shop-Font-end/src/test/java/lk/ijse/gdse/shoeshopfontend/ShoeShopFontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.shoeshopfontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeShopFontEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
